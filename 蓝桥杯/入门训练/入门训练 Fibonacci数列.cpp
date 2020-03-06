d25f1fac7ab56602753b9637bec6b3b0c8d958f9
HANXU <1076998404@qq.com>
2020-03-06 17:57:27 +0800

    【入门蓝桥杯】2020年3月6日17:57:11
    今天开始刷蓝桥杯习题

---
+++ 蓝桥杯/入门训练/入门训练 Fibonacci数列.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+	long long int n;
+	cin>>n;
+	int sum=1;
+	int a=1;
+	int b=1;
+	int t=1;
+	if(n>2){
+		for(int i=2;i<=n;i++){
+			t=sum;
+			sum=(a+b)%10007;
+			a=b;
+			b=t;
+		}
+		cout<<sum%10007;
+	}else{
+		cout<<1;
+	}
+	
+
+	return 0;
+}
